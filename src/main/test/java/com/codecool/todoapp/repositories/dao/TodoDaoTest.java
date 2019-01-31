86bb26195d8850dd4ae89bd1a24e0070b5c4be2e
Uj Levente <asdlevente@gmail.com>
2019-01-31 17:59:32 +0100

    database for testing changed from postgres to H2

--- src/main/test/java/com/codecool/todoapp/repositories/dao/TodoDaoTest.java
+++ src/main/test/java/com/codecool/todoapp/repositories/dao/TodoDaoTest.java
@@ -7,6 +7,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -20,7 +21,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @DataJpaTest
 @ActiveProfiles("test")
 @RunWith(SpringRunner.class)
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 public class TodoDaoTest {
 
     @Autowired
@@ -60,7 +61,6 @@ public class TodoDaoTest {
         todoDao.update(testTodo.getId(), newTitle);
 
         String title = todoRepository.findAll().get(0).getTitle();
-        System.out.println(title + "||||||||||||||||||||||||||");
         assertThat(title).isEqualTo(newTitle);
     }
 }
